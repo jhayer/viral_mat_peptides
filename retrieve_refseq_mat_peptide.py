0df37c65af0cf25ce6d7786fce4e1536b286b812
Juliette Hayer <juliette.hayer@gmail.com>
2020-06-29 16:07:46 +0200

    cleaned code and added comments

--- retrieve_refseq_mat_peptide.py
+++ retrieve_refseq_mat_peptide.py
@@ -8,7 +8,6 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC
 
-
 def main():
     parser = argparse.ArgumentParser(description="To retrieve gbk entries that\
      contain mat_peptide features from a gbk file containing several entries")
@@ -38,12 +37,14 @@ def filter_entries(in_gbk, out_gbk, fasta_out):
 
     for gb_record in SeqIO.parse(gb_handle, "genbank"):
         new_rec = gb_record
-        #print("Name %s, %i features" % (gb_record.name, len(gb_record.features)))
 
         product_mat_peptide = index_genbank_features(gb_record, "mat_peptide", "product")
         if product_mat_peptide:
-        #    print(product_mat_peptide)
+            # writing the full record to the output gb file if the entry \
+            # contains mat_peptide with product qualifier
             SeqIO.write(new_rec, save_handle, "genbank")
+            # if an output fasta file is specified we retrieve the mp
+            # translations in the fasta file
             if fasta_out!=None:
                 get_mat_peptide_translation(new_rec, fasta_handle)
 
@@ -52,7 +53,6 @@ def filter_entries(in_gbk, out_gbk, fasta_out):
     fasta_handle.close()
 
 def get_mat_peptide_translation(gb_record,fasta_handle):
-    #fasta_handle = open(fasta_out, 'w')
 
     acc_num = gb_record.id
     taxid = 'none'
@@ -64,32 +64,27 @@ def get_mat_peptide_translation(gb_record,fasta_handle):
         if feature.type == 'source':
             if 'db_xref' in feature.qualifiers:
                 s_db_xref = feature.qualifiers['db_xref'][0]
-                print(s_db_xref)
+            #    print(s_db_xref)
                 if s_db_xref.startswith('taxon'):
+                    #/db_xref="taxon:2169854"
                     taxid = s_db_xref.split(':')[1]
 
         elif feature.type == 'mat_peptide':
-            # retrieve the mat_peptide seq from location and translate
-            #print(feature.location)
+            # retrieve the mat_peptide seq from the location and translate
             if feature.location.strand == +1:
                 mp_seq = gb_record.seq[feature.location.start:feature.location.end].translate(table=1)
             else:
                 mp_seq = gb_record.seq[feature.location.start:feature.location.end].reverse_complement().translate(table=1)
-
+            #then retrieve the product qualifier to get a name for the mat_peptide sequence
             if 'product' in feature.qualifiers:
                 s_prod = feature.qualifiers['product'][0].replace(" ","-")
-
+            # the sequence header will be: acc_taxid_mp-product-name
             key = "%s_%s_%s" % (acc_num, taxid, s_prod)
-
-            #print(key)
-            #print(mp_seq)
+            # THen we create a new sequence object SeqRecord
             mp_record = SeqRecord(Seq(str(mp_seq), IUPAC.protein),
                     id=key, name=acc_num+str(index), description="")
-        #    mp_record.description(key)
-        #    mp_record.seq(mp_seq)
+            # writing seq to fasta file
             SeqIO.write(mp_record, fasta_handle, "fasta")
-
-    #fasta_handle.close()
 
 
 def index_genbank_features(gb_record, feature_type, qualifier) :
